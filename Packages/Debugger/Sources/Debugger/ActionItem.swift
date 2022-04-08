7ac90566698e172e79d8bdc35d3cc586dc64d8f1
BB9z <bb9z@me.com>
2022-04-09 07:59:23 +0800

    DebugActionItem 变为专有结构

--- Packages/Debugger/Sources/Debugger/ActionItem.swift
+++ Packages/Debugger/Sources/Debugger/ActionItem.swift
@@ -12,54 +12,48 @@
 import UIKit
 
 /// 调试操作定义
-public typealias DebugActionItem = UIBarButtonItem
+public struct DebugActionItem {
+    /// 操作描述
+    public let title: String
+    weak var target: AnyObject?
+    var action: Selector?
+    var actionBlock: (() -> Void)?
+
+    /// 使用闭包创建一个调试操作
+    public init(_ title: String, action: (() -> Void)?) {
+        self.title = title
+        self.actionBlock = action
+    }
+
+    /// target/action 方式创建一个调试操作
+    public init(_ title: String, target: AnyObject?, _ action: Selector) {
+        self.title = title
+        self.target = target
+        self.action = action
+    }
+}
 
 /// 在 view controller 中定义，debugger 会从当前页面逐级向上遍历以获取所有的调试操作
-@objc public protocol DebugActionSource {
+public protocol DebugActionSource {
     /// 返回当前页面层级中支持的调试操作
     func debugActionItems() -> [DebugActionItem]
 }
 
-public extension DebugActionItem {
-    /// 使用闭包创建一个调试操作
-    convenience init(_ title: String, action: (() -> Void)?) {
-        self.init(title: title, style: .plain, target: nil, action: nil)
-        actionBlock = action
-    }
-
-    /// target/action 传统方式创建一个调试操作
-    convenience init(_ title: String, target: Any?, _ action: Selector) {
-        self.init(title: title, style: .plain, target: target, action: action)
-    }
-}
-
-/// 增加对闭包的支持
 internal extension DebugActionItem {
-    private var actionBlock: (() -> Void)? {
-        get {
-            objc_getAssociatedObject(self, &actionBlockAssociation) as? (() -> Void)
-        }
-        set {
-            objc_setAssociatedObject(self, &actionBlockAssociation, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
-        }
-    }
-
     /// 执行附加的操作
     func perform() {
-        if !isEnabled { return }
         actionBlock?()
         if let sel = action {
             UIApplication.shared.sendAction(sel, to: target, from: nil, for: nil)
         }
     }
 }
-private var actionBlockAssociation: UInt8 = 0
 
 // swiftlint:disable identifier_name
 
 /// 旧版接口
-public func DebugMenuItem(_ title: String, _ target: Any?, _ selector: Selector) -> DebugActionItem {
-    DebugActionItem(title: title, style: .plain, target: target, action: selector)
+public func DebugMenuItem(_ title: String, _ target: AnyObject?, _ selector: Selector) -> DebugActionItem {
+    DebugActionItem(title, target: target, selector)
 }
 
 /// 旧版接口
